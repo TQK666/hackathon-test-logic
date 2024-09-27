fd1ec0b630162328abceb6455f1d0002c4778133
Khanh <khanhtq5@ocb.com.vn>
2024-09-27 09:20:20 +0700

    add project

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,24 @@
-fun getItem(value1: Int, weight1: Int, value2: Int, weight2: Int, maxWeight: Int): Int {
+fun maxTreasureValue(value1: Int, weight1: Int, value2: Int, weight2: Int, maxW: Int): Int {
     return when {
-        weight1 + weight2 <= maxWeight -> value1 + value2
-        maxWeight in weight1..<weight2 -> value1
-        maxWeight in weight2..<weight1 -> value2
-        weight1 > maxWeight && weight2 > maxWeight -> 0
-        else -> if (value1 > value2) value1 else value2
+        // Case 1: Can take both items
+        weight1 + weight2 <= maxW -> value1 + value2
+
+        // Case 2: Can only take the first item
+        maxW in weight1..<weight2 -> value1
+
+        // Case 3: Can only take the second item
+        maxW in weight2..<weight1 -> value2
+
+        // Case 4: Can take either item, return the one with the higher value
+        weight1 <= maxW && weight2 <= maxW -> maxOf(value1, value2)
+
+        // Case 5: Can't take any item
+        else -> 0
     }
 }
 
 fun main() {
-    val result = getItem(10, 5, 6, 4, 8) // expect 10
-    val result1 = getItem(10, 5, 6, 4, 9) // expect 16
-    val result2 = getItem(5, 3, 7, 4, 6) // expect 7
-    val result3 = getItem(3, 5, 3, 8, 10) // expect 3
-    println(result)
-    println(result1)
-    println(result2)
-    println(result3)
+    println(maxTreasureValue(10, 5, 6, 4, 8))  // Output: 10
+    println(maxTreasureValue(10, 5, 6, 4, 9))  // Output: 16
+    println(maxTreasureValue(5, 3, 7, 4, 6))   // Output: 7
 }
